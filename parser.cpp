36217838a090375a0cc696e130039a38546a5098
zamir5895 <lmr293141@gmail.com>
2025-06-19 14:03:04 -0500

    updates for parser.h and parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "parser.h"
+#include "expression.h"
+#include "scanner.h"
+#include "token.h"
+using namespace std;
+
+bool Parser::match(Token::Type ttype) {
+    if (check(ttype)) {
+        advance();
+        return true;
+    }
+    return false;
+}
+
+bool Parser::check(Token::Type ttype) {
+    if (isAtEnd()) return false;
+    return current->type == ttype;
+}
+
+bool Parser::advance() {
+    if (!isAtEnd()) {
+        Token* temp = current;
+        if (previous) delete previous;
+        current = scanner->nextToken();
+        previous = temp;
+        if (check(Token::ERROR)) {
+            cout << "Error de análisis, carácter no reconocido: " << current->text << endl;
+            exit(1);
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Parser::isAtEnd() {
+    return (current->type == Token::END_OF_FILE);
+}
+Parser::Parser(Scanner* sc):scanner(sc) {
+    previous = NULL;
+    current = scanner->nextToken();
+    if (current->type == Token::ERROR) {
+        cout << "Error en el primer token: " << current->text << endl;
+        exit(1);
+    }
+}
+
--- parser.h
+++ parser.h
@@ -1,16 +1,73 @@
-//
-// Created by zamirlm on 6/5/25.
-//
-
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "expression.h"
+#include "scanner.h"
 
+class Parser {
+private:
+    Scanner* scanner;
+    Token *current, *previous;
 
-class parser {
+    bool match(Token::Type ttype);
+    bool check(Token::Type ttype);
+    bool advance();
+    bool isAtEnd();
+    void consume(Token::Type ttype, const string& message);
+    Token* consumeIdent(const string& message);
 
+    Exp* parseExpression();
+    Exp* parseAssignment();
+    Exp* parseLogicalOr();
+    Exp* parseLogicalAnd();
+    Exp* parseEquality();
+    Exp* parseComparison();
+    Exp* parseAdditive();
+    Exp* parseMultiplicative();
+    Exp* parseUnary();
+    Exp* parsePostfix();
+    Exp* parsePrimary();
+    Exp* parseFunctionCall();
+    Exp* parseArrayAccess();
+    Exp* parseMemberAccess();
+    Exp* parseNumberExp();
+    Type* parseType();
+
+    Stm* parseStatement();
+    Stm* parseIfStatement();
+    Stm* parseWhileStatement();
+    Stm* parseForStatement();
+    Stm* parseReturnStatement();
+    Stm* parsePrintStatement();
+    Stm* parsePrintfStatement();
+    Stm* parseExpressionStatement();
+    Stm* parseVarDeclaration();
+
+    VarDec* parseVarDec();
+    VarDecList* parseVarDecList();
+    GlobalVarDec* parseGlobalVarDec();
+    Parameter* parseParameter();
+    ParameterList* parseParameterList();
+
+    Body* parseBody();
+    Function* parseFunction();
+    StructDeclaration* parseStructDeclaration();
+    Include* parseInclude();
+
+public:
+    Parser(Scanner* scanner);
+    ~Parser();
+
+    Program* parseProgram();
+    IncludeList* parseIncludes();
+    GlobalVarDecList* parseGlobalDeclarations();
+    StructDeclarationList* parseStructDeclarations();
+    FunctionList* parseFunctions();
+    MainFunction* parseMainFunction();
+
+    StatementList* parseStatementList();
+    list<Exp*> parseArguments();
+    list<Exp*> parseInitializerList();
 };
 
-
-
-#endif //PARSER_H
+#endif // PARSER_H
